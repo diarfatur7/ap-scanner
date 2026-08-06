520cff5eb04872b65fa81a52cfeef91faf0b7e57
diarfatur7 <diarfatur7@gmail.com>
2026-08-06 14:11:11 +0700

    Update script.js

--- script.js
+++ script.js
@@ -49,3 +49,110 @@ async function startCamera(){
     }
 
 }
+
+async function scan(){
+
+    scanBtn.disabled = true;
+
+    status("Mengambil gambar...");
+
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+
+    const ctx = canvas.getContext("2d");
+
+    ctx.drawImage(
+        video,
+        0,
+        0,
+        canvas.width,
+        canvas.height
+    );
+
+    function playBeep(){
+
+    try{
+
+        const audio = new Audio(
+        "https://actions.google.com/sounds/v1/cartoon/wood_plank_flicks.ogg"
+        );
+
+        audio.play();
+
+    }catch(e){
+
+        console.log(e);
+
+    }
+
+}
+
+function vibrate(){
+
+    if(navigator.vibrate){
+
+        navigator.vibrate(200);
+
+    }
+
+}
+
+    const base64 = canvas
+        .toDataURL("image/jpeg",0.9)
+        .replace(/^data:image\/jpeg;base64,/,"");
+
+    status("Mengirim ke OCR...");
+
+    try{
+
+        const response = await fetch(WEBAPP_URL,{
+
+            method:"POST",
+
+            headers:{
+                "Content-Type":"application/json"
+            },
+
+            body:JSON.stringify({
+
+                image:base64
+
+            })
+
+        });
+
+        const json = await response.json();
+
+        console.log(json);
+
+        if(json.success){
+
+            hasil.innerHTML = json.nomor;
+
+            status(json.message);
+
+            playBeep();
+
+            vibrate();
+
+        }else{
+
+            hasil.innerHTML = "-";
+
+            status(json.message);
+
+        }
+
+    }catch(err){
+
+        console.error(err);
+
+        hasil.innerHTML = "-";
+
+        status("Gagal terhubung ke Apps Script");
+
+    }
+
+    scanBtn.disabled = false;
+
+}
